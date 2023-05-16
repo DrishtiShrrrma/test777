cf2496a08aa5d9d3a8b1749a5301c0db588a3a1b
Drishti Sharma <129742046+DrishtiShrrrma@users.noreply.github.com>
2023-05-17 00:03:51 +0530

    Update Agent.py

--- MyAgent/Agent.py
+++ MyAgent/Agent.py
@@ -11,7 +11,7 @@
 
 
 
-Attempt 1:
+# Attempt 1:
     
 import random
 
@@ -37,3 +37,52 @@ class NoOpAgent:
             action["action_values"] = action_values
         
         return action
+    
+   
+# Attempt 2:
+
+import numpy as np
+from scipy.optimize import minimize
+
+class NoOpAgent:
+    def __init__(self, action_space, num_actions=0, horizon=5):
+        self.action_space = action_space
+        self.num_actions = num_actions
+        self.horizon = horizon
+
+    def sample_action(self, state=None):
+        # Defining MPC algorithm 
+        # we can use scipy.optimize.minimize to optimize an objective function
+        
+        action = {}
+        
+        if self.action_space == "continuous":
+            # Define the objective function to be minimized
+            def objective_function(action_values):
+                # Compute the cost or reward associated with the action sequence based on the current state and future predictions
+                
+                # Needs to be replaced!
+                cost = 0
+                for t in range(self.horizon):
+                    cost += some_cost_function(state, action_values[t])
+                
+                return cost
+            
+            # Set the bounds for action values
+            bounds = [(0, 1)] * self.num_actions
+            
+            # Initialize the initial guess for action values
+            initial_guess = np.random.uniform(0, 1, self.num_actions * self.horizon)
+            
+            # Use scipy.optimize.minimize to find the optimal action values
+            result = minimize(objective_function, initial_guess, bounds=bounds)
+            
+            # Extract the optimal action values
+            optimal_action_values = result.x.reshape((self.horizon, self.num_actions))
+            
+            # Set the action to the first optimal action values
+            action["action_values"] = optimal_action_values[0]
+        
+        return action
+
+
